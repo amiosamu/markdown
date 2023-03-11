e37dc2a0e39224ddf0e58630bf159c909f4042be
amiosamu <damitullaev@gmail.com>
2023-03-11 16:47:10 +0600

    User model updated

--- model/user.go
+++ model/user.go
@@ -6,5 +6,5 @@ type User struct {
 	UID      uuid.UUID `db:"uid" json:"uid"`
 	Name     string    `db:"name" json:"name"`
 	Email    string    `db:"email" json:"email"`
-	Password string    `db:"password" json:"password"`
+	Password string    `db:"password" json:"-"`
 }
