0480f1676a879b5d53f0682765c719ec8c60a26a
amiosamu <damitullaev@gmail.com>
2023-03-11 23:42:56 +0600

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -2,28 +2,40 @@ package main
 
 import (
 	"context"
+	"github.com/amiosamu/markdown/pkg/database"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
-
-	"github.com/amiosamu/markdown/handler"
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
 	log.Println("starting server...")
-	engine := gin.Default()
 
-	handler.NewHandler(&handler.Config{
-		Engine: engine,
-	})
+	db, err := database.InitDB()
+
+	if err != nil {
+		log.Fatalf("unable to init database: %v\n", err)
+	}
+
+	router, err := inject(db)
+
+	if err != nil {
+		log.Fatalf("unable to inject data sources %v\n", err)
+	}
+	//
+	//engine := gin.Default()
+	//
+	//handler.NewHandler(&handler.Config{
+	//	Engine: engine,
+	//})
+
 	srv := &http.Server{
 		Addr:    ":8080",
-		Handler: engine,
+		Handler: router,
 	}
 	go func() {
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
@@ -37,8 +49,13 @@ func main() {
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
+	if err := db.Close(); err != nil {
+		log.Fatalf("a problem occured gracefully shuttding down data sources: %v\n", err)
+	}
+
 	log.Println("Shutting down server...")
 	if err := srv.Shutdown(ctx); err != nil {
 		log.Fatalf("Server forced to shutdown: %v\n", err)
 	}
+
 }
