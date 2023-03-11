c22113b6dbcdd47399d93587216e551cdf7fd7ed
amiosamu <damitullaev@gmail.com>
2023-03-11 17:33:00 +0600

    Init User service

---
+++ service/user_service.go
@@ -0,0 +1,41 @@
+package service
+
+import (
+	"context"
+	"github.com/amiosamu/markdown/model"
+	"github.com/amiosamu/markdown/pkg/apperrors"
+	"github.com/google/uuid"
+	"log"
+)
+
+type UserService struct {
+	UserRepository model.UserRepository
+}
+
+type USConfig struct {
+	UserRepository model.UserRepository
+}
+
+func NewUserService(c *USConfig) model.UserService {
+	return &UserService{
+		UserRepository: c.UserRepository,
+	}
+}
+
+func (s *UserService) Get(ctx context.Context, uid uuid.UUID) (*model.User, error) {
+	u, err := s.UserRepository.FindByID(ctx, uid)
+	return u, err
+}
+
+func (s *UserService) Signup(ctx context.Context, u *model.User) error {
+	pw, err := hashPassword(u.Password)
+	if err != nil {
+		log.Printf("unable to sign up user for email: %v\n", u.Email)
+		return apperrors.NewInternalServerError()
+	}
+	u.Password = pw
+	if err := s.UserRepository.Create(ctx, u); err != nil {
+		return err
+	}
+	return nil
+}
