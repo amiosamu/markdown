4ad92051ae04b5651cf0f82e99cc3d82713b9959
amiosamu <damitullaev@gmail.com>
2023-03-11 17:12:27 +0600

    mocks

---
+++ model/mocks/token_service.go
@@ -0,0 +1,24 @@
+package mocks
+
+import (
+	"context"
+	"github.com/amiosamu/markdown/model"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockTokenService struct {
+	mock.Mock
+}
+
+func (m *MockTokenService) NewPairFromUser(ctx context.Context, u *model.User, prevToken string) (*model.TokenPair, error) {
+	ret := m.Called(ctx, u, prevToken)
+	var r0 *model.TokenPair
+	if ret.Get(0) != nil {
+		r0 = ret.Get(0).(*model.TokenPair)
+	}
+	var r1 error
+	if ret.Get(1) != nil {
+		r1 = ret.Get(1).(error)
+	}
+	return r0, r1
+}
